0cfa89f0663ebe2f9b9fbca4a6220b9d4651c265
Gio d'Amelio <giodamelio@gmail.com>
2020-12-28 01:32:30 -0600

    Do errors with Boom

--- src/entrypoints/fetch-feed.ts
+++ src/entrypoints/fetch-feed.ts
@@ -1,35 +1,37 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import fetch from 'node-fetch';
+import { badRequest } from '@hapi/boom';
+
 import 'source-map-support/register';
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  if (!event.body) {
+  try {
+    if (!event.body) {
+      throw badRequest('No body');
+    }
+
+    const body = JSON.parse(event.body);
+
+    if (!body.channelId) {
+      throw badRequest('No channelId');
+    }
+
+    const baseUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=';
+
+    const feedRes = await fetch(`${baseUrl}${body.channelId}`);
+    const feed = await feedRes.text();
+
     return {
-      statusCode: 400,
+      statusCode: 200,
+      body: feed,
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode,
       body: JSON.stringify({
-        message: 'No body',
+        error: err.error,
+        message: err.message,
       }),
     };
   }
-
-  const body = JSON.parse(event.body);
-
-  if (!body.channelId) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: 'No channelId',
-      }),
-    };
-  }
-
-  const baseUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=';
-
-  const feedRes = await fetch(`${baseUrl}${body.channelId}`);
-  const feed = await feedRes.text();
-
-  return {
-    statusCode: 200,
-    body: feed,
-  };
 }
