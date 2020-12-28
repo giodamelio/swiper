a124eede7ff9aa48f5b154aeba892c6088d6eaaf
Gio d'Amelio <giodamelio@gmail.com>
2020-12-27 20:45:29 -0600

    Fetch channel feed

--- src/entrypoints/fetch-feed.ts
+++ src/entrypoints/fetch-feed.ts
@@ -1,12 +1,35 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import fetch from 'node-fetch';
 import 'source-map-support/register';
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'No body',
+      }),
+    };
+  }
+
+  const body = JSON.parse(event.body);
+
+  if (!body.channelId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'No channelId',
+      }),
+    };
+  }
+
+  const baseUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=';
+
+  const feedRes = await fetch(`${baseUrl}${body.channelId}`);
+  const feed = await feedRes.text();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Fetching feed!',
-      input: event,
-    }, null, 2),
+    body: feed,
   };
 }
