da16be37824276f71d822180514325258a5d73df
Gio d'Amelio <giodamelio@gmail.com>
2020-12-28 13:08:55 -0600

    Parse Feed XML

--- src/entrypoints/fetch-feed.ts
+++ src/entrypoints/fetch-feed.ts
@@ -1,8 +1,21 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import fetch from 'node-fetch';
 import { badRequest } from '@hapi/boom';
+import { parseString } from 'xml2js';
 
 import 'source-map-support/register';
+
+function parseXML(input: string): Promise<Record<string, any>> {
+  return new Promise((resolve, reject) => {
+    parseString(input, { explicitArray: false }, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
   try {
@@ -24,11 +37,12 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
       throw badRequest('Bad response from YouTube');
     }
 
-    const feed = await feedRes.text();
+    const feedText = await feedRes.text();
+    const feed = await parseXML(feedText);
 
     return {
       statusCode: 200,
-      body: feed,
+      body: JSON.stringify(feed.feed.entry.map(({title}) => title)),
     };
   } catch (err) {
     return {
