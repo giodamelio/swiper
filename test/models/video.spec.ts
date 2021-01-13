c5759ce8c428e10a23fc2766fbbc3505e162227c
Gio d'Amelio <giodamelio@gmail.com>
2021-01-13 10:36:27 -0600

    Use the static Video.create function to make things less verbose

--- test/models/video.spec.ts
+++ test/models/video.spec.ts
@@ -10,15 +10,14 @@ describe('models', () => {
     it('save a single video', async () => {
       expect.assertions(1);
 
-      const coolSubmarineVideo = new Video();
-      coolSubmarineVideo.id = 'bPJUVKizh90';
-      coolSubmarineVideo.title =
-        'How to Make Pizza on a Submarine - Smarter Every Day 246';
-      coolSubmarineVideo.description = '<too long>';
-      coolSubmarineVideo.thumbnail =
-        'https://i3.ytimg.com/vi/bPJUVKizh90/hqdefault.jpg';
-      coolSubmarineVideo.published = new Date('2020-11-01T17:10:34+00:00');
-      coolSubmarineVideo.updated = new Date('2020-12-18T09:13:39+00:00');
+      const coolSubmarineVideo = Video.create({
+        id: 'bPJUVKizh90',
+        title: 'How to Make Pizza on a Submarine - Smarter Every Day 246',
+        description: '<too long>',
+        thumbnail: 'https://i3.ytimg.com/vi/bPJUVKizh90/hqdefault.jpg',
+        published: new Date('2020-11-01T17:10:34+00:00'),
+        updated: new Date('2020-12-18T09:13:39+00:00'),
+      });
       await coolSubmarineVideo.save();
 
       expect(await Video.count()).toBe(1);
